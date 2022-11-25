e36929e1a6671f626ac09a657c392a9d21fb1340
KIIKE\enriq <enriquevilas9@gmail.com>
2022-11-25 13:40:56 +0100

    Se sube el ejercicio completo

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// Variables
+// letiables
 let numerosMostrados = 0;
 let boton1 = null;
 let boton2 = null;
@@ -10,19 +10,20 @@ let accParejas = 6;
 // Elementos HTML
 let contadorMovs = document.getElementById("contadorMovs");
 let parejasRestantes = document.getElementById("parejasRestantes");
+let tabla = document.getElementById("tabla");
 
 // Generacion de numero
 let numeros = [];
 
-function devolverNumerosDeCuadrados(horizontal , vertical){
+function devolverNumerosDeCuadrados(filas , columnas){
 
-    var totalNums = horizontal * vertical;
+    let totalNums = filas * columnas;
     // Comrpobamos si el total de casillas es par para poder generar los numeros
     if((totalNums % 2) == 0){
         // Si el resultado es un numero par, calculamos el total de numeros a generar 
         // Por ejemplo, el tablero es de 3 X 4 => 12 casillas. Al ser numero par, lo dividimos entre 2 
         // y obtenemos el numero maximo de parejas (6)
-        var num = totalNums/2;
+        let num = totalNums/2;
 
         for (let i = 1; i <= num; i++) {
             numeros.push(i);
@@ -37,54 +38,83 @@ function devolverNumerosDeCuadrados(horizontal , vertical){
     return numeros;
 }
 
-numeros = devolverNumerosDeCuadrados(3 , 4);
+numeros = devolverNumerosDeCuadrados(4 , 3);
 numeros = numeros.sort(()=> {return Math.random() - 0.5});
 
 function obtenerDificultad(event) { 
+
     event.preventDefault();
     // Funcion para obtener la dificultad seleccioanda 
     // Una vez obtenemos la dificultad, calculamos los numeros
     
-    numeros = []; // Se reinicia el valor de la lista de numeros para que no se arrastren numeros a mayores
+    // Al pulsar el boton para seleccionar la dificultar , se elimina el tablero por defecto para generar uno nuevo segun los datos seleccionados
+    while (tabla.firstChild) { 
+        tabla.removeChild(tabla.firstChild);
+    }
+    // Cada vez que seleccionamos un nivel nuevo, reiniciamos el contador de movimientos
+    contadorMovs.innerHTML = 0; 
 
+    // Se reinicia el valor de la lista de numeros para evitar posibles errores
+    numeros = []; 
+    
+    // Comprobamos que dificulad se selecciono y generamos el tablero y numeros
     if (document.getElementById("facil").checked) {
-        // console.log("facil");
+
+        genera_tabla(3 , 4);
         numeros = devolverNumerosDeCuadrados(3 , 4);
         numeros = numeros.sort(()=> {return Math.random() - 0.5});
 
         accParejas = (numeros.length/2); // 6
         parejasRestantes.innerHTML = accParejas;
-
-        console.log(accParejas);
     }
 
     if (document.getElementById("medio").checked) {
-        //console.log("medio");
+
+        genera_tabla(5 , 4);
         numeros = devolverNumerosDeCuadrados(4 , 5);
         numeros = numeros.sort(()=> {return Math.random() - 0.5});
 
         accParejas = (numeros.length/2); // 10
         parejasRestantes.innerHTML = accParejas;
-        console.log(accParejas);
     }
 
     if (document.getElementById("dificil").checked) {
         //console.log("dificil");
+        genera_tabla(6 , 6);
         numeros = devolverNumerosDeCuadrados(6 , 6); // 18
         numeros = numeros.sort(()=> {return Math.random() - 0.5});
 
         accParejas = (numeros.length/2);
         parejasRestantes.innerHTML = accParejas;
-        console.log(accParejas);
+    }
+
+    if (document.getElementById("custom").checked) {
+
+        // Obtenemos el valor de los campos de texto 
+        let filas = document.getElementById("filas").value;
+        let columnas = document.getElementById("columnas").value;
+
+
+        // Comprobamos que la multiplicacion de un numero para para calcular las parejas
+        if ((filas * columnas) % 2 == 0) {
+            // Generamos la tabla y la lista de numeros en funcion de los datos introducidos
+            genera_tabla(filas , columnas);
+            numeros = devolverNumerosDeCuadrados(filas , columnas); 
+            numeros = numeros.sort(()=> {return Math.random() - 0.5});
+    
+            accParejas = (numeros.length/2);
+            parejasRestantes.innerHTML = accParejas;
+
+        } else {
+            alert("Numero no validos");
+        }
     }
 }
 
-// let numerosDesordenados = numeros.sort(()=> {return Math.random() - 0.5});
-// console.log("Numero DESordenados:" + numeros);
 
 function mostrarNum(idBoton) {
 
-    numerosMostrados++; // Variable de control para saber cuantos botones se han pulsado
+    numerosMostrados++; // variable de control para saber cuantos botones se han pulsado
 
     if (numerosMostrados == 1) {
         // Recogermos el boton con el id que se le pasa como parametro
@@ -111,17 +141,14 @@ function mostrarNum(idBoton) {
         contadorMovs.innerHTML = accMovs;
 
         if (resultado1 == resultado2) {
-            /* 
-            En caso de que lo numeros sean iguales reseteamos el contador de numeros mostrados a 0 y mostramos que queda 1 pareja menos por buscar
-            */
+            // En caso de que lo numeros sean iguales reseteamos el contador de numeros mostrados a 0 y mostramos que queda 1 pareja menos por buscar
             numerosMostrados = 0;
             accParejas--;
             parejasRestantes.innerHTML = accParejas;
 
         } else {
-            /* 
-            En caso de no sean iguales reseteamos el contador de numeros mostrados a 0 y mostramos que queda 1 pareja menos por buscar
-            */
+            // En caso de no sean iguales reseteamos el contador de numeros mostrados a 0 y mostramos que queda 1 pareja menos por buscar
+
             setTimeout(()=>{
                 boton1.innerHTML = " ";
                 boton1.disabled = false;
@@ -133,4 +160,39 @@ function mostrarNum(idBoton) {
             numerosMostrados = 0;
         }
     }
+}
+
+function genera_tabla(filas , columnas) {
+    // referencia a los elementos
+    let tablero = document.getElementById("tablero")
+    let tabla = document.getElementById("tabla");
+
+    let id = 0; // Id que se le asignara a cada boton
+
+    // Creamos cada fila
+    for (let i = 0; i < filas; i++) {
+      // Crea las filas de la tabla
+        let fila = document.createElement("tr");
+
+        for (let j = 0; j < columnas; j++) {
+            // En cada una de las filas crearemos el elemento <td> al que se le añadira un boton
+            let celda = document.createElement("td");
+            let botonCelda = document.createElement("button");
+
+            // Cada vez que se crea un boton se le asignara un id la funcion onClick que tendra asignada
+            botonCelda.setAttribute("id" , id);
+            botonCelda.setAttribute("onClick" , "mostrarNum("+id+")");
+            // Una vez listo el boton, lo añadiremos a la celda, y esta, a la fila
+            celda.appendChild(botonCelda);
+            fila.appendChild(celda);
+
+            // Y sumaremos 1 al contador de id's
+            id++;
+        }
+
+        // Se agrega la tabla
+        tabla.appendChild(fila);
+    }
+    // Una vez la tabla ya esta creada, la añadimos a la seccion "tablero"
+    tablero.appendChild(tabla);
 }
